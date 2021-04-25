be3a69bd41dd9ee9ec67ba986b36c8ba76288a95
Mattia Zingaretti <zingaretti.mattia@gmail.com>
2021-04-25 15:33:25 +0200

    Done esercitazione 04. Query corrette 4/11 ...

--- Lab04/Query10.sql
+++ Lab04/Query10.sql
@@ -1,13 +1,14 @@
--- Chiamiamo «standardizzata» una birra che è venduta
--- allo stesso prezzo in tutti i bar. Calcolare le fabbriche
--- di birra che fabbricano almeno una birra non
--- standardizzata
+-- Mostrare i garage che hanno custodito
+-- solo automobili immatricolate a Roma.
+
+SELECT DISTINCT C.codgarage
+FROM custodita C 
+JOIN automobile A ON C.targaauto=A.targa
+WHERE 'Roma' = all (
+                                    SELECT A1.citta 
+                                    FROM custodita C1
+                                    JOIN automobile A1 ON A1.targa=C1.targaauto
+                                    WHERE C1.codgarage=C.codgarage
+                                 )
 
 
-SELECT DISTINCT B.fabbrica
-FROM birra B,serve S,(		
-	SELECT AVG(serve.prezzo)::NUMERIC(10,2) as mean
-	FROM serve
-	GROUP BY serve.birra
-) T
-WHERE B.nome=S.birra AND T.mean != S.prezzo
--- Lab04/Query11.sql
+++ Lab04/Query11.sql
@@ -1,9 +1,13 @@
---Mostrare i garage attivi che hanno custodito
--- solo automobili immatricolate a Roma
+-- Mostrare i garage che hanno custodito
+--  solo automobili immatricolate nella stessa
+-- città in cui trova il garage
+
+
 SELECT DISTINCT C.codgarage
 FROM custodita C 
-JOIN automobile A ON C.targaauto=A.targa
-WHERE C.numero > 0 AND 'Roma' = all (
+JOIN automobile A ON C.targaauto=A.targa 
+JOIN garage G ON G.codice=C.codgarage
+WHERE G.citta = all (
                                     SELECT A1.citta 
                                     FROM custodita C1
                                     JOIN automobile A1 ON A1.targa=C1.targaauto
